e24269d9d3214a6d39bbbcee981a522e7e19e96d
xavierpicciotto <58976717+xavierpicciotto@users.noreply.github.com>
2020-10-21 16:04:18 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const saucesRoutes = require('./routes/sauces');
+const userRoutes = require('./routes/user');  
+
+mongoose.connect('mongodb+srv://XavAdmin:azer7894@cluster0.4z6rm.mongodb.net/ProjetTest?retryWrites=true&w=majority', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('Connexion à MongoDB réussi !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+const app = express();
+
+app.use((req, res, next) => {
+   res.setHeader('Access-Control-Allow-Origin', '*');
+   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+   next();
+});
+
+app.use(bodyParser.json());
+
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use('/api/sauces', saucesRoutes);
+app.use('/api/auth', userRoutes);
+
+module.exports = app;
