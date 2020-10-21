123b10a1ab5a7477e072f9f30d03b5a4ba5e9762
xavierpicciotto <58976717+xavierpicciotto@users.noreply.github.com>
2020-10-21 16:05:08 +0200

    Create sauces.js

---
+++ routes/sauces.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+const saucesCtrl = require('../controllers/sauces');
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
+
+router.get('/', auth, saucesCtrl.findAllSauces);
+router.post('/', auth , multer, saucesCtrl.createSauce);
+router.put('/:id', auth, multer, saucesCtrl.updateSauce);
+router.get('/:id', auth, saucesCtrl.findOneSauce);
+router.delete('/:id', auth, saucesCtrl.deleteSauce);
+router.post('/:id/like', auth, saucesCtrl.ratingSauce);
+
+module.exports = router;
