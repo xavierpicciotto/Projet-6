3061696c65e18adbb973a5e7182023aa1bc6c406
xavierpicciotto <58976717+xavierpicciotto@users.noreply.github.com>
2020-10-22 18:21:43 +0200

    Update sauces.js

--- controllers/sauces.js
+++ controllers/sauces.js
@@ -3,6 +3,7 @@ const {
     db
 } = require('../models/Sauces');
 
+//Pour la création d'une sauce.
 exports.createSauce = (req, res, next) => {
     const saucesObject = JSON.parse(req.body.sauce);
     console.log(saucesObject);
@@ -19,6 +20,7 @@ exports.createSauce = (req, res, next) => {
         }));
 };
 
+//Récupère toutes les sauces.
 exports.findAllSauces = (req, res, next) => {
     Sauces.find()
         .then(sauces => res.status(200).json(sauces))
@@ -27,6 +29,7 @@ exports.findAllSauces = (req, res, next) => {
         }));
 };
 
+//Récupère une seule sauce.
 exports.findOneSauce = (req, res, next) => {
     Sauces.findOne({
         _id: req.params.id
@@ -43,6 +46,7 @@ exports.findOneSauce = (req, res, next) => {
     );
 };
 
+//Permet de modifier une sauce enregistrée, image / information.
 exports.updateSauce = (req, res, next) => {
     const sauceObject = req.file ? {
         ...JSON.parse(req.body.sauce),
@@ -64,6 +68,7 @@ exports.updateSauce = (req, res, next) => {
         }));
 };
 
+//Suprime une sauce.
 exports.deleteSauce = (req, res, next) => {
 
     Sauces.deleteOne({
@@ -83,18 +88,24 @@ exports.deleteSauce = (req, res, next) => {
     );
 };
 
+//Importe la fonction des likes et dislikes.
 const voted = require('./like_modules/voted');
 
+//Permet de Liker ou disliker une sauce ou bien de retirer son choix.
 exports.ratingSauce = (req, res, next) => {
     const user = req.body.userId;
     const voteValue = req.body.like;
     console.log(req.body);
+    //On récupère l'objet de la sauce.
     Sauces.findOne({
         _id: req.params.id
     }).then(
         (sauce) => {
+            //On utilisa la fonction importée pour renvoyer les modification a apporter.
             const rating = voted(sauce, voteValue, user);
             console.log(JSON.stringify(rating) + " . . . check object update");
+
+            //On modifie la sauce avec les nouvelles informations.
             Sauces.updateOne({
                     _id: req.params.id
                 }, {
