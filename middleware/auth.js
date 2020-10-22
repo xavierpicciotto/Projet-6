aceff9633b989e7148a607bb673e3b1c3795da3a
xavierpicciotto <58976717+xavierpicciotto@users.noreply.github.com>
2020-10-22 18:21:48 +0200

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,10 +1,12 @@
 const jwt = require('jsonwebtoken');
 
+//Module JWT pour la comunication JSON sécurisée.
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(' ')[1];
     const decodedToken = jwt.verify(token, 'le_Token_est_YGDYURYGHhuzhdk654s84s65z7shdi');
     const userId = decodedToken.userId;
+    //Vérifie si l'utilisateur est correctement identifié.
     if (req.body.userId && req.body.userId !== userId) {
       throw 'Invalid user ID';
     } else {
