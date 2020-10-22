1962d2c759fbac8b53582eaa582a7d7b6ea406c9
xavierpicciotto <58976717+xavierpicciotto@users.noreply.github.com>
2020-10-22 18:21:40 +0200

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const path = require('path');
 const saucesRoutes = require('./routes/sauces');
 const userRoutes = require('./routes/user');  
 
+// Connection a MongoDB Atlas.
 mongoose.connect('mongodb+srv://XavAdmin:azer7894@cluster0.4z6rm.mongodb.net/ProjetTest?retryWrites=true&w=majority', {
         useNewUrlParser: true,
         useUnifiedTopology: true
@@ -15,6 +16,7 @@ mongoose.connect('mongodb+srv://XavAdmin:azer7894@cluster0.4z6rm.mongodb.net/Pro
 
 const app = express();
 
+// Headers pour les cors(Cross Origine Resource Sharing).
 app.use((req, res, next) => {
    res.setHeader('Access-Control-Allow-Origin', '*');
    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -24,9 +26,10 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 
-
+// Dossier où enregistrer les images.
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
+//Utilisation des routes de l'API.
 app.use('/api/sauces', saucesRoutes);
 app.use('/api/auth', userRoutes);
 
