70e2249c7fb3f89d8d34bfb8cdf9b14de77aa9bd
xavierpicciotto <58976717+xavierpicciotto@users.noreply.github.com>
2020-10-22 18:21:58 +0200

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+
+//Importation du fichier des controllers.
 const userCtrl = require('../controllers/user');
 
 router.post('/signup', userCtrl.signup);
